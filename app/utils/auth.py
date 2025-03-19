32bb7aa438c2d6f1041937a45c7efabb83203a3d
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-19 14:16:49 -0300

    refactor: added role system on middleware

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,20 +1,29 @@
 import jwt
+from functools import wraps
 from flask import request, jsonify
 from app.config import Config
 
-def token_required(f):
-    def decorator(*args, **kwargs):
-        token = request.headers.get('Authorization')
-        if not token:
-            return jsonify({'error': 'Token is missing!'}), 401
-        
-        try:
-            token = token.split(" ")[1] 
-            data = jwt.decode(token, Config.SECRET_KEY, algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            return jsonify({'error': 'Token has expired!'}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({'error': 'Invalid token!'}), 401
+def role_required(allowed_roles):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            token = request.headers.get('Authorization')
+            if not token:
+                return jsonify({'error': 'Token is missing!'}), 401
+            
+            try:
+                token = token.split("Bearer ")[-1]
+                decoded_token = jwt.decode(token, Config.SECRET_KEY, algorithms=['HS256'])
 
-        return f(data, *args, **kwargs)
+                
+                if decoded_token.get('role') not in allowed_roles:
+                    return jsonify({'error': 'Unauthorized access'}), 403
+
+                return f(*args, **kwargs)
+            except jwt.ExpiredSignatureError:
+                return jsonify({'error': 'Token has expired'}), 401
+            except jwt.InvalidTokenError:
+                return jsonify({'error': 'Invalid token'}), 401
+
+        return decorated_function
     return decorator
