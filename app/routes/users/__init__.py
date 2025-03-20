0c4479740906e5c97856ba1ee658593d5aea84bf
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-20 02:36:58 -0300

    feat: enable CORS support for users blueprint with credential support

--- app/routes/users/__init__.py
+++ app/routes/users/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
+from flask_cors import CORS
 
 users = Blueprint('users', __name__)
+
+CORS(users, supports_credentials=True)
 
 from . import create_user, list_users, get_user, delete_user, login, create_user_mod
